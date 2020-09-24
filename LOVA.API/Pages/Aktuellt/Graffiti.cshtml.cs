af4a9d78858bef90f1f169429b56d973e241b33c
Johan <johan@tempelman.nu>
2020-09-24 21:46:58 +0200

    Added news pages.

---
+++ LOVA.API/Pages/Aktuellt/Graffiti.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LOVA.API.Pages.Aktuellt
+{
+    public class GraffitiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
