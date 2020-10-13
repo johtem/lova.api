ed18c284605d9fad937f255b1b659e4a3421491d
Johan Tempelman <johan@tempelman.nu>
2020-10-13 13:23:45 +0200

    Removed Telerik Scheduler in the Rental section

--- LOVA.API/ViewModels/RentalSchedularViewModel.cs
+++ LOVA.API/ViewModels/RentalSchedularViewModel.cs
@@ -2,6 +2,7 @@
 using LOVA.API.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,8 +15,39 @@ namespace LOVA.API.ViewModels
         public int RentalInventoryId { get; set; }
         public string Title { get; set; }
 
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        private DateTime start;
+
+        [Required]
+        public DateTime Start
+        {
+            get
+            {
+                return start;
+            }
+            set
+            {
+                start = value.ToUniversalTime();
+            }
+        }
+
+   
+
+
+        private DateTime end;
+
+        [Required]
+        [DateGreaterThan(OtherField = "Start")]
+        public DateTime End
+        {
+            get
+            {
+                return end;
+            }
+            set
+            {
+                end = value.ToUniversalTime();
+            }
+        }
         public string Description { get; set; }
 
         public string BackgroundColor { get; set; }
@@ -24,13 +56,16 @@ namespace LOVA.API.ViewModels
         public string EndTimezone { get; set; }
         public string RecurrenceRule { get; set; }
         public string RecurrenceException { get; set; }
+        public int? RecurrenceID { get; set; }
 
         public int NumberOf { get; set; }
 
         public string AspNetUserId { get; set; }
 
-        public int? RoomID { get; set; }
 
         public IEnumerable<int> Attendees { get; set; }
+
+
+
     }
 }
