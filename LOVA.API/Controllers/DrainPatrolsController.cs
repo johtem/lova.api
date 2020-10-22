3a18da48d6faa51291b2d24a8954e39de489383d
Johan Tempelman <johan@tempelman.nu>
2020-10-22 11:18:31 +0200

    Added KPI API controller

--- LOVA.API/Controllers/DrainPatrolsController.cs
+++ LOVA.API/Controllers/DrainPatrolsController.cs
@@ -133,6 +133,9 @@ namespace LOVA.API.Controllers
         }
 
 
+
+
+
         // POST: api/DrainPatrols
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
